a1d056affcecee0ca07b8b4a42dee203f8a79cd2
Inshiya Ravat <inshiya.ravat@simformsolutions.com>
2025-05-05 18:16:12 +0530

    feature: download resume

--- src/component/Navbar/Navbar.tsx
+++ src/component/Navbar/Navbar.tsx
@@ -1,26 +1,46 @@
 import styles from './Navbar.module.css';
 import Button from '../Button/Button';
 import Menu from '../../assets/Hamburger-menu.svg';
+import Resume from '../../assets/resume.txt';
+import { useState } from 'react';
 
+const ListItem = () =>{
+    return (
+        <>
+            <li className={styles.activeSection}><a href='#'>Home</a></li>
+            <li className={styles.inActiveSection}><a href='#'>About me</a></li>
+            <li className={styles.inActiveSection}><a href='#'>Projects</a></li>
+            <li className={styles.inActiveSection}><a href='#'>Contact me</a></li>
+        </>
+    )
+}
 const Navbar = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    function handleMenu(){
+        setIsMenuOpen(!isMenuOpen);
+    }
   return (
     <div className={styles.navbarContainer}>
         <div className={styles.menu}>
-            <Button variant='transparent'>
+            <Button onClick={handleMenu} variant='transparent'>
                 <img src={Menu} alt='hamburger menu'/>
             </Button>
+            {
+                isMenuOpen && (
+                    <ul className={styles.menuList}>
+                        <ListItem/>
+                    </ul>
+                )
+            }
         </div>
       <div>
         <p className={styles.logo}>INSHIYA RAVAT</p>
       </div>
       <ul className={styles.actionContainer}>
-        <li className={styles.activeSection}>Home</li>
-        <li className={styles.inActiveSection}>Projects</li>
-        <li className={styles.inActiveSection}>About me</li>
-        <li className={styles.inActiveSection}>Contact me</li>
+        <ListItem/>
       </ul>
       <div>
-        <Button variant='filled' title='Download Resume'>Resume</Button>
+        <Button variant='filled' title='Download Resume'><a href={Resume} download>Resume</a></Button>
       </div>
     </div>
   )
